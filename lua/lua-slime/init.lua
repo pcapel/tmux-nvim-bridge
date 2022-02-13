86b2a3b54852da9c5e3f176177b2d63df9a2c194
Philip Capel <insert.available.name@gmail.com>
2022-02-12 20:06:26 -0600

    Get session autocomplete working
    It's probably the hackiest thing that I have ever hackedl... but it
    works. There _must_ be a better way though.

--- lua/lua-slime/init.lua
+++ lua/lua-slime/init.lua
@@ -1,6 +1,14 @@
 local utils = require('lua-slime.utils')
 local vfn = vim.fn
 
+local function as_options(array)
+  options = '['
+  for i=1, #(array) do
+    options = options .. '"' .. array[i] .. '"' .. ','
+  end
+  return options .. ']'
+end
+
 -- returns a table with the values
 -- session_name: the current tmux session
 -- window_index: the currently active window index
@@ -22,27 +30,46 @@ local function active_target()
   }
 end
 
-local function tmux_windows()
+-- Get the windows for a given session
+local function tmux_windows(session)
   if vim.g.tslime_always_current_window then
-    return active_target.window_index
+    return active_target().window_index
+  else
+    return utils.arr_line(vfn.system('tmux list-windows -F "#{window_index}" -t ' .. session))
+  end
+end
+
+local function tmux_sessions()
+  if vim.g.tslime_always_current_session then
+    return active_target().session_name
   else
     return utils.arr_line(vfn.system('tmux list-sessions -F "#{session_name}"'))
   end
 end
 
--- local function send_key_to_tmux(key, tmux_target)
---   if not vim.fn.exists('g:tslime') then
---     tmux_vars()
---   end
+local function get_session_name(names)
+  -- TODO: figure out a better way to do this
+  vim.cmd("function! Options(A,L,P)\n" .. "return " .. as_options(names) .. "\nendfunction")
 
---   local res = vim.cmd(':call system("tmux send-keys -t ' .. tmux_target .. key .. '")')
--- end
+  local user_input = ''
+  while user_input == '' do
+    user_input = vfn.input('session name: ', '', 'customlist,Options')
+  end
+  return user_input
+end
 
+local function tmux_vars()
+  local session_names = tmux_sessions()
+  vim.g.tslime = {}
+  if #(session_names) == 1 then
+    vim.g.tslime['session'] = names[1]
+  else
+    vim.g.tslime['session'] = get_session_name(session_names)
+  end
+end
 
 local function run_test()
-  for key, value in pairs(tmux_windows()) do
-    print(key, value)
-  end
+  get_session_name(tmux_sessions())
 end
 
 return {
